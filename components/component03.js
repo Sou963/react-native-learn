f53f09bb0925bb2045561599b307d2ce6e58926f
Sourav Bosu <121472055+Sou963@users.noreply.github.com>
2026-04-13 01:55:08 +0600

    Add files via upload

---
+++ components/component03.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class component03 extends Component {
+  render() {
+    return (
+      <View>
+        <Text> this is component03 </Text>
+      </View>
+    )
+  }
+}
