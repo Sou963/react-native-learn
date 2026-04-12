82301c0bb9feeb07fa7eb17a7a0d7618a80c94bd
Sourav Bosu <121472055+Sou963@users.noreply.github.com>
2026-04-13 01:52:34 +0600

    Add files via upload

--- App.js
+++ App.js
@@ -1,43 +1,76 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import FixedDimensionsBasics from "./components/class01";
-import Class02 from "./components/class02";
-import Class03 from "./components/class03";
-import Class04 from "./components/class04";
-import Class05 from "./components/class05";
-import Class06 from "./components/class06";
-import State from "./components/state";
-import Styling from "./components/styling";
-import Flexbox from "./components/flexbox";
-import List from "./components/list";
-
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-import Home from "./components/home";
-import About from "./components/about";
-
-const Stack = createNativeStackNavigator();
+import { Text, View, StyleSheet } from "react-native";
+import Csscom from "./css_com";
+import Page_com from "./page/page_com";
+import State from "./page/state";
+import Setstate from "./page/setstate";
+import FunctionalState from "./page/func_state";
+import Props_fun from "./page/props_fun";
+import Class_props from "./page/class_props";
+// import Compontent from "./page/compontent";
+import AlertExample from "./page/alerts";
+import Flexdesign from "./page/design";
+import Scrollviews from "./page/scrollviews";
 
 export class App extends Component {
   render() {
+    // const mystyle=StyleSheet.create({
+    //   red:{
+    //     color:'red',
+    //     fontWeight: "bold"
+    //   },
+    //   purple:{
+    //     color:'purple',
+    //     fontWeight:'bold'
+    //   }
+
+    // });
     return (
-      <NavigationContainer>
-        {/* Navbar if needed */}
-        {/* <Navbar /> */}
-        <State />
+      <View style={{ flex: 1 }}>
+        {/* <Page_com/> */}
+        {/* <State/> */}
+        {/* <Setstate /> */}
+        {/* <FunctionalState /> */}
+        {/* <Props_fun name="John" age={25} gender="male" /> */}
+        {/* <Class_props name="John" age={25} gender="male" /> */}
+        {/* <Compontentomponent /> */}
 
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          {/* <Stack.Screen name="Navbar" component={Navbar} /> */}
+        {/* <Flexdesign /> */}
 
-          <Stack.Screen name="Home" component={Home} />
+        <Scrollviews />
 
-          <Stack.Screen name="About" component={List} />
-        </Stack.Navigator>
-        {/* <Home /> */}
-      </NavigationContainer>
+        {/* <Csscom/> */}
+        {/* <Text style={mystyle.red}> this is red </Text>
+       <Text style={mystyle.purple}> this is purple </Text> */}
+      </View>
     );
   }
 }
 
 export default App;
+
+// import React, { Component } from "react";
+// import { Text, View } from "react-native";
+// import { AlertExample } from "./App";
+
+// export class ComponentSome extends Component {
+//   render() {
+//     return (
+//       <View
+//         style={{
+//           flex: 1, // This tells the view to fill the entire screen
+//           flexDirection: "row",
+//           backgroundColor: "teal",
+//           justifyContent: "center",
+//           alignItems: "center",
+//         }}
+//       >
+//         <Text>a</Text>
+//         <Text>b</Text>
+//         <Text>c</Text>
+//       </View>
+//     );
+//   }
+// }
+
+// export default ComponentSome;
